1e7562ce4a46e850bb2ad170ddad31f18d7ed9f8
ravindu644 <droidcasts@protonmail.com>
2026-06-09 12:05:12 +0000

    build script: nuked the obsolete Samsung build script and replaced it with ours
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
 
+echo -e "\n[INFO]: BUILD STARTED..!\n"
 
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r
+# Init submodules
+git submodule update --init --recursive
+
+# Export core variables
+export KERNEL_ROOT="$(pwd)"
 export ARCH=arm64
 
-make ARCH=arm64 exynos9820-m62xx_defconfig
-make ARCH=arm64 -j16
+# Prepare build environment
+mkdir -p "${KERNEL_ROOT}/out" "${KERNEL_ROOT}/build" "${HOME}/toolchains"
+
+# Export toolchain paths
+export PATH="${HOME}/toolchains/clang-r510928/bin:${PATH}"
+export LD_LIBRARY_PATH="${HOME}/toolchains/clang-r510928/lib:${LD_LIBRARY_PATH}"
+export BUILD_CC="${HOME}/toolchains/clang-r510928/bin/clang"
+
+# Build options for the kernel
+export BUILD_OPTIONS=(
+    -C "${KERNEL_ROOT}"
+    O="${KERNEL_ROOT}/out"
+    -j"$(nproc)"
+    ARCH=arm64
+    LLVM=1
+    LLVM_IAS=1
+    HOSTCC=gcc
+    HOSTCXX=g++
+    CC="${BUILD_CC}"
+    CLANG_TRIPLE=aarch64-linux-gnu-
+)
+
+build_kernel(){
+    # Make default configuration.
+    make "${BUILD_OPTIONS[@]}" exynos9820_defconfig beyondx.config ksu.config
+
+    # Configure the kernel (GUI)
+    make "${BUILD_OPTIONS[@]}" menuconfig
+
+    # Build the kernel
+    make "${BUILD_OPTIONS[@]}" Image || exit 1
+
+    # Copy the built kernel to the build directory
+    cp "${KERNEL_ROOT}/out/arch/arm64/boot/Image" "${KERNEL_ROOT}/build"
+
+    echo -e "\n[INFO]: BUILD FINISHED..!"
+}
+build_kernel
